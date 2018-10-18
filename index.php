e967a507bc7b087950d2f4f0f37539abac6b9e21
stefanomorena <stefanomoren@gmail.com>
2018-10-18 13:12:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,18 +24,27 @@ and open the template in the editor.
         $a->setTime('0', '0', '0');
         $a->setDate('2017', '11', '11');
         $pren->setDate($a);
-        print $pren->getNumOmbrellone();
-        
-        //print $a->format("d-m-Y H:i:s");
-       
-        $fpren=new FPrenotazione();
-        $fpren->caricaPrenotazione($pren);
-        
-        
-        //$fpren->connetti($prova->get_hostname(), $prova->get_dbname(), $prova->get_user(), $prova->get_pass());
+        $b=$a;
+        $prova=new FPrenotazione();
+        //$prova->inserisci($pren);
+        $prova->cerca("id", 4);
+        //$prova->cancella("id", 3);
         
         
-        //$prova->connetti($prova->get_hostname(), $prova->get_dbname(), $prova->get_user(), $prova->get_pass());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+   
         
         
         
