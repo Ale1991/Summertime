e1352c5a930326c2ac58aef708e0f1493abecbd2
Alessio <alessio.hurricane@gmail.com>
2018-12-31 14:29:48 +0100

    fix

--- summertimeapp/src/api/v1/utenti/startSession.php
+++ summertimeapp/src/api/v1/utenti/startSession.php
@@ -121,39 +121,3 @@ function startSession(Request $request, Response $response)
     }
 
 }
-
-/*     try
-{
-$futente = new FUtente();
-$utente = $futente->getObject($nomeUtente);
-if ($utente->getPassword() == $password) {
-
-if (!isset($_SESSION['nomeUtente']) || session_id() == '') {
-
-$_SESSION['nomeUtente'] = $nomeUtente;
-$_SESSION['id'] = session_id();
-$message = [
-"text" => 'Successfully logged!',
-"session" => $_SESSION,
-];
-} else {
-$_SESSION['nomeUtente'] = $nomeUtente;
-$_SESSION['id'] = session_id();
-$message = [
-"text" => 'Already Logged',
-"session" => $_SESSION,
-];
-
-}
-
-} else {
-$message = [
-"text" => 'Nome Utente o Password Errati!',
-"session_id" => null,
-];
-
-}
-echo json_encode($message);
-} catch (PDOException $e) {
-echo ' {"error":{"text": ' . $e->getMessage() . '}';
-} */
